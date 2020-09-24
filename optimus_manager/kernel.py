0af30485ca6cdfdf31624a905231d375c0b133ec
Heinrich Malan <heinrich@heinbio.me>
2020-09-24 20:51:26 +0200

    unbind virt consoles when downing nvidia also unload i2c_nvidia_gpu

--- optimus_manager/kernel.py
+++ optimus_manager/kernel.py
@@ -180,7 +180,9 @@ def _wait_no_processes_on_nvidia():
 
 
 def _unload_nvidia_modules(available_modules):
-    _unload_modules(available_modules, ["nvidia_drm", "nvidia_modeset", "nvidia_uvm", "nvidia"])
+    exec_bash("echo 0 > /sys/class/vtconsole/vtcon0/bind")
+    exec_bash("echo 0 > /sys/class/vtconsole/vtcon1/bind")
+    _unload_modules(available_modules, ["i2c_nvidia_gpu", "nvidia_drm", "nvidia_modeset", "nvidia_uvm", "nvidia"])
 
 def _unload_nouveau(available_modules):
     exec_bash("echo 0 > /sys/class/vtconsole/vtcon0/bind")
